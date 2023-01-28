cbf4d028b4fc5245f24a69f80be788553ccea5c9
Cieciak <nikodemrzasa@wp.pl>
2023-01-28 07:21:02 +0100

    change: samples

--- client.py
+++ client.py
@@ -1,8 +1,6 @@
-import CPPP, time
+# The simplest client
+import CPPP
 
-msg = CPPP.CPPPMessage(header = {'method': 'GET', 'localiztion': '/'}, body = b'Hello World!')
-response = CPPP.send_request('127.0.0.1', 8000, msg)
-print(response.decode('utf-8'))
-
+msg = CPPP.CPPPMessage(header = {'method': 'GET', 'localiztion': '/'}, body = b'get')
 response = CPPP.send_request('127.0.0.1', 8000, msg)
 print(response.decode('utf-8'))
--- server.py
+++ server.py
@@ -1,5 +1,8 @@
+# The simplest echo server
+
 import CPPP
 
 server = CPPP.CPPPServer('0.0.0.0', 8000)
+
 try: server.serve()
 except KeyboardInterrupt: print('Server stopped!')
