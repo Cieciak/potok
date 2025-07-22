60f198237b5bcace95c310356cf9c87959b2ab07
Cieciak <nikodemrzasa@wp.pl>
2025-07-22 23:51:42 +0200

    py-potok scripts

---
+++ py-potok/scripts/init.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+if [ -d .venv ]; then
+  echo "Python venv already exists"
+else
+  python3 -m venv .venv
+fi
+source "$(pwd)/.venv/bin/activate"
+pip3 install -r requirements.txt
+
+if [ "$1" == "dev" ]; then
+  echo "Developer env"
+  pip3 install -e .
+fi
